b95b26188c201072d87547b5fbcfa140ddba8cb9
delawwww <2223591@smkn-2sbg.sch.id>
2025-02-20 11:29:31 +0700

    menambahkan route search

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TaskListController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
+
 // Membuat route untuk home
 // Route untuk halaman utama aplikasi, memanggil method 'index' dari TaskController
 Route::get('/', [TaskController::class, 'index'])->name('home');
@@ -20,3 +21,5 @@ Route::resource('tasks', TaskController::class);
 Route::patch('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
 
 Route::patch('/tasks/{task}/change-list', [TaskController::class, 'changeList'])->name('tasks.changeList');
+//Route untuk mengelola data yang ingin di search
+Route::get('/search', [SearchController::class, 'search'])->name('search');
